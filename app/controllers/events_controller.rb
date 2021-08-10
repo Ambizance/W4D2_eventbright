aa5c50c0fefe2bcf71bedbcc7dc7b89ccd13f96a
Ambizance <ambroise.breant@gmail.com>
2021-08-10 18:07:28 -0400

    commit 2.3

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,50 @@
 class EventsController < ApplicationController
   def index
+    if params[:user_id]
+      @events= User.find(params[:user_id]).events
+    else
+      @events= Event.all
+    end
   end
+
+  def show
+
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @event = Event.new(admin_id:current_user.id)
+    @event.update(post_params)
+
+    if @event.save
+      redirect_to root_path
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:event).permit(:title, :description, :location, :duration, :price, :start_date)
+    end
+
 end
+
+
